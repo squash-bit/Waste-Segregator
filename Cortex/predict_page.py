0cad46558e4e414636986a556b7d8b997a5f2b23
squash-bit <agbofrederick56@gmail.com>
2021-10-13 19:31:15 +0000

    predict garbage data

--- Cortex/predict_page.py
+++ Cortex/predict_page.py
@@ -1,13 +1,46 @@
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
 import streamlit as st
+import torch
 import pickle
 import numpy as np
 
+data_dir = '/content/garbage classification/Garbage classification'
+
+transformations = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+dataset = ImageFolder(data_dir, transform = transformations)
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
 def load_model():
+    filename = 'finalized_model.pkl'
     with open(filename, 'rb') as file:
         saved_model = pickle.load(file)
     return saved_model
 
+device = get_default_device()
 loaded_model = load_model()
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    prob, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return dataset.classes[preds[0].item()]
 
 def predict_garbage(image_name):
     image = Image.open(Path('./' + image_name))
